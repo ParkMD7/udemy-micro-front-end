711f57d9a0c98ed59a12b833a7cdd5886027304a
Parker Dinsmore <parker@kustomer.com>
2023-04-27 01:08:18 -0400

    check invalidation

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from './components/MarketingApp';
 
 export default () => {
   return <div>
-    <h1>hello world</h1>
+    <h1>hello world!</h1>
     <hr />
     <MarketingApp />
   </div>
