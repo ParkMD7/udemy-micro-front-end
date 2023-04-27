1beb001245cd38ae262e37d1e6b9fcb5e5663669
Parker Dinsmore <parker@kustomer.com>
2023-04-27 16:13:46 -0400

    prevent classname collisions

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,16 +1,24 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 
 import Header from './components/Header';
 import MarketingApp from './components/MarketingApp';
 
+// prevent auto-generated css class name collisions between micro-frontends
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'co',
+})
+
 export default () => {
   return (
     <BrowserRouter>
-      <div>
-        <Header />
-        <MarketingApp />
-      </div>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </StylesProvider>
     </BrowserRouter>
-  )
+  );
 };
