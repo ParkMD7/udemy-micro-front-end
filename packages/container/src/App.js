407e339ae491b2ed4aa86933226729945709b80d
Parker Dinsmore <parker@kustomer.com>
2023-04-27 18:47:40 -0400

    add basic auth and auth nav

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 
 import Header from './components/Header';
 import MarketingApp from './components/MarketingApp';
+import AuthApp from './components/AuthApp';
 
 // prevent auto-generated css class name collisions between micro-frontends
 const generateClassName = createGenerateClassName({
@@ -16,7 +17,10 @@ export default () => {
       <StylesProvider generateClassName={generateClassName}>
         <div>
           <Header />
-          <MarketingApp />
+          <Switch>
+            <Route path="/auth" component={AuthApp} />
+            <Route path="/" component={MarketingApp} />
+          </Switch>
         </div>
       </StylesProvider>
     </BrowserRouter>
